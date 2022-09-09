c05551eff547a073fdce2387cae5e8d32a80ca39
DataSdude <ayoubjbili98@gmail.com>
2022-09-08 19:34:37 -0700

    My first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Program that prints to stdout with puts function*/
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
