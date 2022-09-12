1fee691363e8ba79106a6571a6b61c5f6b1ac2dd
DataSdude <ayoubjbili98@gmail.com>
2022-09-11 18:08:34 -0700

    C program that checks isa random number is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+/*
+ * main : display a simple text if the number n is positive or negative
+ * description: this program  checks if a random number is positive or negative
+ * return 0
+ */
+
+int main(void)
+
+{
+
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if(n<0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is positive\n", n);
+
+ 	return (0);
+
+}
