d4f2f7eb7917a26999e8aa642b9ff1043eb4c53c
DataSdude <ayoubjbili98@gmail.com>
2022-09-12 17:10:40 -0700

    Program that prints all single numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+/**
+ * * main - Entry point
+ *
+ * * description: program to print the numbers of base 10
+ *
+ * * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i< 10; i++)
+		putchar(i + 48);
+
+	putchar('\n');
+
+	return (0);
+}
