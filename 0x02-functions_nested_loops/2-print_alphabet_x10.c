b3077dc520f35b914e5cbd960bbcb5ddae0cc746
DataSdude <ayoubjbili98@gmail.com>
2022-09-13 07:32:16 -0700

    Program that prints the alphabet 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+
+/**
+ *
+ *  *print_alphabet - utilizes on the _putchar function
+ *
+ *   *to print the alphabet a - z  ten times
+ *
+ *    */
+
+
+
+void print_alphabet_x10(void)
+{
+	int i,j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j< 26; j++)
+			_putchar(j + 'a');
+		_putchar('\n');
+	}
+
+}
