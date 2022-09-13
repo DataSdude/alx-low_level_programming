f0ba9b4382369939c5547d1f11619cea71a7ce72
DataSdude <ayoubjbili98@gmail.com>
2022-09-13 07:21:05 -0700

    Program that prints lowercase alohabets using function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,20 +3,20 @@
 
 
 /**
- *  * print_alphabet - utilizes on the _putchar function 
- *  to print the alphabet a - z
+ *print_alphabet - utilizes on the _putchar function
+ *to print the alphabet a - z
  */
 
 void print_alphabet(void)
 {
-	int i =0;
+	int i = 0;
 
 	while (i < 26)
 	{
 		_putchar(i + 'a');
-		
+
 		i++;
 	}
-	
+
 	_putchar('\n');
 }
