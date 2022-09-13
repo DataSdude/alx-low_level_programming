6932e405e09030aae2bafd3b821723178b643650
DataSdude <ayoubjbili98@gmail.com>
2022-09-13 06:18:20 -0700

    Program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
-#include <stdlib.h>
-
-#include <stdio.h>
+#include "main.h"
 
 /**
  * * main - Entry point
- *
- * * description: program to print _putchar
- *
+ * * description: program to print _putchar using putchar prototype
  * * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+
+	int x;
+
+	for (x = 0; x < 8; ++x)
+
+		_putchar(str[x]);
+
+	_putchar('\n');
+
 	return (0);
 }
