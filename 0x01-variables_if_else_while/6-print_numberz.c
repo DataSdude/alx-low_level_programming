a6cb3f7c40f232959d0ed76bd2bd0a0f53603082
DataSdude <ayoubjbili98@gmail.com>
2022-09-12 17:17:12 -0700

    Program that prints all single numbers of base 10 without using a char variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+/**
+ *
+ * * main - Entry point
+ *
+ * * description: program to print all single digit numbers of base 10
+ *
+ * * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		putchar( i + 48);
+
+	putchar('\n');
+
+	return (0);
+}
