e417f01096770e8f7c81ffa1af842b5d0dbd3bf4
DataSdude <ayoubjbili98@gmail.com>
2022-09-13 19:55:37 -0700

    Program that prints addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/*
+ * add - add two integers
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: sum of a & b
+ */
+
+int add (int a, int b)
+{
+	int s = a + b;
+
+	return (s);
+
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/* 
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int n= 0;
+
+	n = add(89, 9);
+
+	printf("%d\n", n);
+
+	return (0);
+
+}
