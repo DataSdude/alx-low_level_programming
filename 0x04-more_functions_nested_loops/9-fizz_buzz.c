b6159e9660f2f381805c9372bcae72d4eb11d80b
DataSdude <ayoubjbili98@gmail.com>
2022-09-15 16:18:21 -0700

    Fizz Buzz function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,22 @@ void fizz_buzz(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz ");
-		if (i % 5 == 0 && i % 3 != 0)
-			printf("Buzz ");
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz ");
-		if (i % 3 != 0 && i % 5 != 0)
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (i % 5 ==0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else
 			printf("%d ", i);
 	}
 	printf("\n");
