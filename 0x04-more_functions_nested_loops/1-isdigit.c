95b1d3fed2557e6b05e4874e402467b3fda7c45a
DataSdude <ayoubjbili98@gmail.com>
2022-09-15 11:11:08 -0700

    Program to check if a character is number

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 /*
- * *_isdigit - func to check if a char is digit
+ * *_isdigit - func to check if a char is digit or not
  * *@c - input to check agaisnt
  * *Return - 1 if true 0 if not
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c<= 57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
