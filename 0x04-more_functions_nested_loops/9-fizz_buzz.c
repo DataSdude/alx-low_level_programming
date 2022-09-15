3dfdabdcefbf29786b16c3485fc870d266b0898f
DataSdude <ayoubjbili98@gmail.com>
2022-09-15 16:43:05 -0700

    Fizz Buzz function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,33 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * fizz_buzz - function of wellknown fizzbuzz written in c
- * retuen - none
+ * main - function of wellknown fizzbuzz written in c
+ * return - Always 1
  */
 
-void fizz_buzz(void)
+int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 1; i < 101; i++)
+	for (num = 1; num <= 100; ++num)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-			continue;
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-			continue;
-		}
-		else if (i % 5 ==0)
-		{
-			printf("Buzz ");
-			continue;
-		}
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+
 		else
-			printf("%d ", i);
+			printf("%d", num);
+
+
+		if (num != 100)
+			printf(" ");
+
+		else
+			printf("\n");
 	}
-	printf("\n");
+	
+	return (0);
 }
+
