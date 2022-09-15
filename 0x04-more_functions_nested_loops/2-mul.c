9c827a3d70534afd46733705232988a81ba0c48d
DataSdude <ayoubjbili98@gmail.com>
2022-09-15 11:30:06 -0700

    Program that prints multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/*
+ * *mul - program that do multiplication of two numbers
+ * *@a - input
+ * *@b - input
+ * *Return - multiplication of two numbers a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
