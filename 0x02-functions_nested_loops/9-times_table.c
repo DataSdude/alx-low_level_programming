79f9acfcd8c6bf72f17a6324572ff93f66fce2af
DataSdude <ayoubjbili98@gmail.com>
2022-09-14 17:43:29 -0700

    Program that prints 9times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /*
- * *times_table: prints the 9 times table
+ * times_table - prints the 9 times table
+ * Example Table
+ * 0, 0, 0, 0, ..
+ * 0, 1, 2, 3, ..
  * *Return: nothing
  */
 
