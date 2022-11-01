ca64c8f15081f4ef1736e4bef616266c6065879f
DataSdude <ayoubjbili98@gmail.com>
2022-10-31 18:10:47 -0700

    hope it works

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,40 +17,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	char *buffer;
 
-	if (filename == NULL) /*check if file is present*/
+	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY); /*open file*/
+	file = open(filename, O_RDONLY);
 
 	if (file == -1)
 		return (0);
 
-	/*get the size of buffer from number of letters*/
 
 	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
 	{
 		free(buffer);
-
 		return (0);
 	}
 
-	read_check = read(file, buffer, letters); /*read file*/
+	read_check = read(file, buffer, letters);
 
-	if (read_check == -1) /*check if read failed*/
-
+	if (read_check == -1)
 		return (0);
 
-	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+	wcount = write(STDOUT_FILENO, buffer, read_check);
 
-	if (wcount == -1 || read_check != wcount) /*check if write failed*/
-
+	if (wcount == -1 || read_check != wcount)
 		return (0);
 
 	free(buffer);
 
-	close(file); /*close file*/
+	close(file);
 
 	return (wcount);
 }
